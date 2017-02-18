51de7a5bb22809fdf03f1eb7dd7c4512828c8178
Philip Youssef <philip.youssef@gmail.com>
2017-02-17 16:37:54 -0800

    create map package and graph class

--- src/main/kotlin/com/phyous/simcity/model/Edge.kt
+++ src/main/kotlin/com/phyous/simcity/model/Edge.kt
@@ -1,3 +1,3 @@
-package com.phyous.simcity.graph
+package com.phyous.simcity.model
 
 data class Edge(val id: Long, val nodes: List<Long>, val tags: Map<String, String>)
--- src/main/kotlin/com/phyous/simcity/model/Node.kt
+++ src/main/kotlin/com/phyous/simcity/model/Node.kt
@@ -1,3 +1,3 @@
-package com.phyous.simcity.graph
+package com.phyous.simcity.model
 
 data class Node(val id: Long, val lat: Double, val lng: Double)
