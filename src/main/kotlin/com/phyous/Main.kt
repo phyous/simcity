dd33506e03d0726b474f976c17bdc944d7e3e8eb
Philip Youssef <philip.youssef@gmail.com>
2017-03-26 23:16:22 -0700

    Use coroutines to download map data and show download status in real time.

--- src/main/kotlin/com/phyous/Main.kt
+++ src/main/kotlin/com/phyous/Main.kt
@@ -2,11 +2,13 @@ package com.phyous
 
 import com.phyous.simcity.data.OverpassClient
 import com.phyous.simcity.model.GeoMap
+import kotlinx.coroutines.experimental.*
 import java.io.File
 import java.lang.Exception
+import java.lang.Thread.sleep
 import java.util.*
 
-fun main(args: Array<String>) {
+fun main(args: Array<String>) = runBlocking {
     val selection = Main.selectMap()
 }
 
@@ -22,7 +24,7 @@ class Main {
                     .map { x -> x.canonicalPath.split("/").last() }
         }
 
-        fun selectMap(): GeoMap {
+        suspend fun selectMap(): GeoMap {
             val storedMaps = getStoredMaps()
             println("Welcome to simcity! Let's grab a map and get some directions ...\n" +
                     "Select a map to start:\n" +
@@ -63,8 +65,8 @@ class Main {
         /**
          * Query stdin for a lat/lng bounding box, and use the Overpass client to query & store all relevant map data
          */
-        fun downloadMap(): GeoMap {
-            println("To download a new map, we'll need to form a bounding box with min/max latitude & longitude.\n" +
+        suspend fun downloadMap(): GeoMap {
+        println("To download a new map, we'll need to form a bounding box with min/max latitude & longitude.\n" +
                     "For example a map of manhattan would be: \n" +
                     "lat_min: 40.6932, lng_min:-74.0176\n" +
                     "lat_max: 40.8796, lng_max:-73.8789\n")
@@ -80,11 +82,19 @@ class Main {
             println("Enter a filename to save as (ending with extension *.map):")
             val fileName: String = readValue(Scanner::nextLine, {x -> !x.isNullOrEmpty() && x.endsWith(".map")})
 
-            
-            val ret = OverpassClient.getRoads(OverpassClient.Bounds(latMin, lngMin, latMax, lngMax))
-            ret.fold({ value ->
+            fun getRoadsAsync() = async(CommonPool) {
+                OverpassClient.getRoads(OverpassClient.Bounds(latMin, lngMin, latMax, lngMax))
+            }
+            val defferdRetrun = getRoadsAsync()
+            print("Downloading map: ")
+            while(!defferdRetrun.isCompleted) {
+                print('.')
+                sleep(1000)
+            }
+
+            defferdRetrun.await().fold({ value ->
                 val fullFileOutputPath = dataDir + fileName
-                println("Query successful; storing results in $fileName")
+                println("\nQuery successful; storing results in $fileName")
                 File(fullFileOutputPath).printWriter().use { out ->
                     out.println(value)
                 }
